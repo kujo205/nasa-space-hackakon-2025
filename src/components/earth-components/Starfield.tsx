be26978915c74513487a8f593242c4dd72f67782
kujo205 <kuc8301@gmail.com>
2025-10-04 23:27:43 +0300

    chore: make stars appear futher

--- src/components/earth-components/Starfield.tsx
+++ src/components/earth-components/Starfield.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 function getPoints({ numStars = 500 } = {}) {
   function randomSpherePoint() {
-    const radius = Math.random() * 25 + 25;
+    const radius = Math.random() * 25 + 65;
     const u = Math.random();
     const v = Math.random();
     const theta = 2 * Math.PI * u;
@@ -43,36 +43,34 @@ function getPoints({ numStars = 500 } = {}) {
   const mat = new THREE.PointsMaterial({
     size: 0.2,
     vertexColors: true,
-    map: new THREE.TextureLoader().load(
-      "./circle.png"
-    ),
+    map: new THREE.TextureLoader().load("./circle.png"),
   });
   const points = new THREE.Points(geo, mat);
   function update(t) {
-      points.rotation.y -= 0.0002;
-      let col;
-      const colors = [];
-      for (let i = 0; i < numStars; i += 1) {
-        const p = positions[i];
-        const { update } = p;
-        const bright = update(t);
-        col = new THREE.Color().setHSL(0.6, 0.2, bright);
-        colors.push(col.r, col.g, col.b);
-      }
-      geo.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
-      geo.attributes.color.needsUpdate = true;
+    points.rotation.y -= 0.0002;
+    let col;
+    const colors = [];
+    for (let i = 0; i < numStars; i += 1) {
+      const p = positions[i];
+      const { update } = p;
+      const bright = update(t);
+      col = new THREE.Color().setHSL(0.6, 0.2, bright);
+      colors.push(col.r, col.g, col.b);
     }
-    points.userData = { update };
+    geo.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+    geo.attributes.color.needsUpdate = true;
+  }
+  points.userData = { update };
   return points;
 }
 
-function Starfield () {
+function Starfield() {
   const ref = React.useRef();
-  const points = getPoints({ numStars: 3000 });
+  const points = getPoints({ numStars: 6000 });
   useFrame((state) => {
     const { clock } = state;
     ref.current.userData.update(clock.elapsedTime);
   });
-  return <primitive object={points} ref={ref}/>
+  return <primitive object={points} ref={ref} />;
 }
 export default Starfield;
