b6083fa2de49a67189df78fad54fc5d27d7a2fdd
kujo205 <kuc8301@gmail.com>
2025-10-04 12:38:38 +0300

    chore: add earth to the map

--- src/app/api/populate/route.ts
+++
@@ -1,9 +0,0 @@
-import { populate } from "@/server/helpers/populate";
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  console.log("Optimizing...");
-  populate();
-  console.log("Optimization complete");
-  return NextResponse.json({ message: "Optimization complete" });
-}
